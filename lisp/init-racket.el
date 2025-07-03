286e11fb6a2f481091930b2e986950e6044dcd5c
yubaitw <yubaitw@proton.me>
2025-07-03 13:05:12 +0800

    lisp/init-racket.el: Add yubai/racket-setup and enable LSP in racket-mode.

--- lisp/init-racket.el
+++ lisp/init-racket.el
@@ -1,7 +1,13 @@
 ;;; init-racket.el --- Configuration for Racket  -*- lexical-binding: t -*-
 
+(defun yubai/racket-setup ()
+  "My setup for Racket."
+  (lsp-deferred))
+
 (use-package racket-mode
-  :ensure t)
+  :ensure t
+  :hook
+  (racket-mode . yubai/racket-setup))
 
 (provide 'init-racket)
 ;;; init-racket.el ends here
