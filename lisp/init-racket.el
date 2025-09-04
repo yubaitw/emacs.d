88377532c7270bd4415291f7381499bebd1d2993
yubaitw <yubaitw@gmail.com>
2025-09-04 15:46:21 +0800

    lisp/init-racket.el (yubai/quit-racket-repl): Add quit keybind for racket repl

--- lisp/init-racket.el
+++ lisp/init-racket.el
@@ -4,10 +4,20 @@
   "My setup for Racket."
   (lsp-deferred))
 
+(defun yubai/quit-racket-repl ()
+  (interactive)
+  (racket-repl-exit)
+  (quit-window))
+
 (use-package racket-mode
   :ensure t
   :hook
-  (racket-mode . yubai/racket-setup))
+  (racket-mode . yubai/racket-setup)
+  :general
+  (general-define-key
+   :states 'normal
+   :hook 'racket-repl-mode
+   "q" 'yubai/quit-racket-repl))
 
 (provide 'init-racket)
 ;;; init-racket.el ends here
