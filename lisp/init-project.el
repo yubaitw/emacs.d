003071624fcb037ec6a31be60198aa17a9f2a04d
yubaitw <yubaitw@gmail.com>
2025-09-06 09:27:24 +0800

    lisp/init-project.el: Use `fd` for project file listing locally.

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -2,6 +2,20 @@
 
 (use-package project
   :ensure nil
+  :config
+  (defun yubai/project-files-in-directory (dir)
+    "Use `fd' to list files in DIR."
+    (let* ((default-directory dir)
+           (localdir (file-local-name (expand-file-name dir)))
+           (command (format "fd -H -t f -0 . %s" localdir)))
+      (project--remote-file-names
+       (sort (split-string (shell-command-to-string command) "\0" t)
+             #'string<))))
+
+  (cl-defmethod project-files ((project (head local)) &optional dirs)
+    "Override `project-files' to use `fd' in local projects."
+    (mapcan #'yubai/project-files-in-directory
+            (or dirs (list (project-root project)))))
   :general
   (yubai/leader-def
     :states 'normal
@@ -10,4 +24,3 @@
 
 (provide 'init-project)
 ;;; init-project.el ends here
-
